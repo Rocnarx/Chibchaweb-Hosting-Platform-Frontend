4bb6a645c5aa388b693103584266dad5dd9ffee8
andresjoc <andres131711@gmail.com>
2025-07-28 07:55:08 -0500

    Revert "Steven"

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,10 @@ import Home from './pages/Home'
 import Dominios from './pages/Dominios'
 import Carrito from './pages/Carrito'
 import Footer from './components/Footer'
-import FormularioC from './pages/FormularioRegistroCliente'
-import Login from './pages/Login'
-import Cuenta from './pages/Cuenta'
+import { Routes, Route } from 'react-router-dom'
 
-import { Routes, Route, Navigate } from 'react-router-dom'
-import { useUser } from './Context/UserContext'
 
 function App() {
-  const { usuario } = useUser();
-
   return (
     <>
       <Navbar />
@@ -20,16 +14,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/dominios" element={<Dominios />} />
         <Route path="/carrito" element={<Carrito />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/registro" element={<FormularioC />} />
-        <Route
-          path="/perfil"
-          element={usuario ? <Cuenta /> : <Navigate to="/login" />}
-        />
       </Routes>
       <Footer />
     </>
-  );
+  )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { UserProvider } from './Context/UserContext';
+import App from './App.jsx';
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <UserProvider>
-        <App />
-      </UserProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
