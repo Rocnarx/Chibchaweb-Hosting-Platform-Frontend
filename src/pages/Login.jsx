13f0f841c6ee2c5c4694fa409662347adb019339
Christian Lancheros <135306440+Rocnarx@users.noreply.github.com>
2025-07-28 13:52:59 -0500

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./Login.css";
-import logo from "../components/resources/logo.png";
+import logo from "../Components/resources/logo.png";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "../Context/UserContext"; // Si estás usando contexto
 
