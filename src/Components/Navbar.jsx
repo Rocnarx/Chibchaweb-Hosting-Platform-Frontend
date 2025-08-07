6d360c3ccd4f405883e8dead6719f11f123b6603
Steven Sanchez <brssanchezc@udistrital.edu.co>
2025-08-07 12:38:36 -0500

    Hosting bloqueda para distribuidores

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -78,7 +78,13 @@ function Navbar() {
         <ul className="navbar-menu">
           <li><NavLink to="/" className="nav-link" onClick={handleMenuClick}>Inicio</NavLink></li>
           {/* <li><NavLink to="/dominios" className="nav-link" onClick={handleMenuClick}>Dominios</NavLink></li> */}
-          <li><NavLink to="/planesHosting" className="nav-link" onClick={handleMenuClick}>Hosting</NavLink></li>
+         {usuario?.tipocuenta !== "DISTRIBUIDOR" && (
+  <li>
+    <NavLink to="/planesHosting" className="nav-link" onClick={handleMenuClick}>
+      Hosting
+    </NavLink>
+  </li>
+)}
           <li><NavLink to="/perfil" className="nav-link" onClick={handleMenuClick}>Mi perfil</NavLink></li>
           <li><NavLink to="/DominiosAdquiridos" className="nav-link" onClick={handleMenuClick}>Mis dominios</NavLink></li>
           <li><NavLink to="/soporte" className="nav-link" onClick={handleMenuClick}>Soporte</NavLink></li>
